f557f75109c1b92356ff70a21cd3be900b7aa615
alym3re <gepanao.jonathan12@gmail.com>
2025-05-20 19:05:44 +0800

    POLISHED STUDENT PAGES

--- sw.js
+++ sw.js
@@ -1,11 +1,7 @@
 self.addEventListener('install', (event) => {
   console.log('Service Worker: Installed');
 
-//   evt.waitUntil(  
-//     caches.open(staticCache).then(cache => {
-//         cache.addAll(assets);
-//     })
-// )
+
 });
 
 self.addEventListener('activate', (event) => {
@@ -16,15 +12,4 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => { 
   console.log('Service Worker: Fetching', event.request.url);
-//   event.respondWith(
-//     fetch(event.request)
-//       .catch(() => caches.match(event.request))
-//   );
-
-
-// evt.respondWith(
-//     caches.match(evt.request).then(cacheRes =>{
-//         return cacheRes || fetch(evt.request);
-//     })
-// )
 });
